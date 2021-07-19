ab2aadd44c92aa75d34966568aa77c410696981f
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2021-07-19 23:48:47 +0800

    Create diy-part3.sh

---
+++ diy-part3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#=============================================================
+# Copyright (c) 2021 baiyunxue
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/baiyunxue/Compile
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (Before Update feeds)
+#=============================================================
+
+# 自定义 IP地址
+sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
+
+# 默认开启 WiFi
+sed -i 's/disabled=1/disabled=0/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 自定义 SSID
+#sed -i 's/ssid=OpenWrt/ssid=lantian/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 删除默认密码
+#sed -i "/CYXluq4wUazHjmCDBCqXF/d" openwrt/package/lean/default-settings/files/zzz-default-settings
+
+# 取消bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# Add immortalwrt's Packages
+git clone --depth=1 -b openwrt-18.06 https://github.com/immortalwrt/packages
+git clone --depth=1 -b openwrt-18.06-k5.4 https://github.com/immortalwrt/luci
+cp -r ../luci/applications/luci-app-adguardhome ../package/lean//luci-app-adguardhome
+
+# Add luci-app-ddnsto
+git clone -b master --single-branch https://github.com/281677160/openwrt-package.git
