8066b60f9a678c204857db75305fbb73bc28b0a9
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2021-07-23 20:48:44 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#===============================================================
+# Copyright (c) 2021 baiyunxue
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/baiyunxue/Compile
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#================================================================
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git hackpackage https://github.com/openwrt/packages.git' >>feeds.conf.default
+echo 'src-git hackluci https://github.com/openwrt/luci.git' >>feeds.conf.default
+#echo 'src-git baiyunxue https://github.com/baiyunxue/lede-feeds.git' >>feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git diy https://github.com/CCnut/feed-netkeeper.git;LUCI-LUA-UCITRACK' feeds.conf.default
