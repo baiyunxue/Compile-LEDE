e3d17a04e95a0c111efe8e627ce8aa0207ac6748
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2021-08-11 15:54:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,11 +19,5 @@
 # 自定义 SSID
 #sed -i 's/ssid=OpenWrt/ssid=lantian/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 删除默认密码
-#sed -i "/CYXluq4wUazHjmCDBCqXF/d" openwrt/package/lean/default-settings/files/zzz-default-settings
-
-# 取消bootstrap为默认主题
-#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
 # 自定义app
 git clone https://github.com/baiyunxue/openwrt-app.git package/files
