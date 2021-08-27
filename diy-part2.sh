028486e44500951f99ae80f9cb82e6341ae82930
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2021-08-27 16:34:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,5 +21,5 @@
 
 # 自定义app
 #git clone https://github.com/baiyunxue/openwrt-app.git package/files
-svn co https://github.com/281677160/openwrt-package/trunk/package/luci-app-ddnsto/network/services/ddnsto package/network/services/ddnsto
-svn co https://github.com/281677160/openwrt-package/trunk/package/luci-app-ddnsto/luci/luci-app-ddnsto package/files/luci-app-ddnsto
+svn co https://github.com/linkease/ddnsto-openwrt/trunk/ddnsto package/network/services/ddnsto
+svn co https://github.com/linkease/ddnsto-openwrt/trunk/luci-app-ddnsto package/files/luci-app-ddnsto
